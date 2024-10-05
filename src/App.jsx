1220e1d6413d44b2fde37e9649701c1603819622
eufanzky <eufanzky@gmail.com>
2024-10-05 14:02:23 -0400

    añadiendo tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="bg-red-500">
+        <button onClick={() => setCount((count) => count + 1)} className='bg-red-500'>
           count is {count}
         </button>
         <p>
