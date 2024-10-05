55ee1eb8d87a02823c7a1e15187c324640ce9c8a
eufanzky <eufanzky@gmail.com>
2024-10-05 15:11:46 -0400

    añadiendo tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // Ensure this file contains Tailwind imports if you're using Tailwind CSS
 
-createRoot(document.getElementById('root')).render(
+// Get the root element where the React app will be rendered
+const rootElement = document.getElementById('root')
+
+// Create a root for rendering the React application
+const root = createRoot(rootElement)
+
+// Render the App component wrapped in StrictMode
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Include your source files
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
