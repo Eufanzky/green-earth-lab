19bceb0f1d7a9f6a3ad9e6e4837f5b695d32b2a7
eufanzky <eufanzky@gmail.com>
2024-10-05 22:12:55 -0400

    slider implemented

--- src/pages/Huellas.jsx
+++ src/pages/Huellas.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
 import Navbar from "../components/NavBar";
-
+import Slider from "../components/Slider";
 
 const Huellas = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -23,10 +23,44 @@ const Huellas = () => {
     );
   };
 
+  const slides = [
+    {
+      image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+      description:
+        "El cambio climático es un fenómeno complejo que ha sido influenciado por factores naturales y humanos. ",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+      description:
+        "El cambio climático es un fenómeno complejo que ha sido influenciado por factores naturales y humanos. ",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+      description:
+        "El cambio climático es un fenómeno complejo que ha sido influenciado por factores naturales y humanos. ",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+      description:
+        "El cambio climático es un fenómeno complejo que ha sido influenciado por factores naturales y humanos. ",
+    },
+    {
+      image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+      description:
+        "El cambio climático es un fenómeno complejo que ha sido influenciado por factores naturales y humanos. ",
+    },
+  ];
+
   return (
     <>
       <Navbar />
-            
+      <div className="py-24 px-10">
+        <div className="flex justify-center items-center h-72 bg-[#3F7E44] ">
+          <div className="w-80">
+            <Slider slides={slides} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
