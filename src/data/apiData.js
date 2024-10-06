8410ec252b65ef5665c60d4afc5da097169d16fc
eufanzky <eufanzky@gmail.com>
2024-10-06 11:37:47 -0400

    api

--- src/data/apiData.js
+++ src/data/apiData.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_KEY = "AIzaSyCfL9zAYIx1bwT3kcOjkCLe17PtpfkOqUQ"; // Usa la variable de entorno o una clave por defecto
+const API_KEY = "your-gemini-api-key"; // Usa la variable de entorno o una clave por defecto
 
 export default API_KEY;
