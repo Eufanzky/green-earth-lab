53bb474bf6edf72bcc565596e0ec8f5cd637c1a0
eufanzky <eufanzky@gmail.com>
2024-10-05 21:13:32 -0400

    huellas

--- src/pages/Huellas.jsx
+++ src/pages/Huellas.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
 import Navbar from "../components/NavBar";
-import Swiper from "../components/Swiper";
 
 
 const Huellas = () => {
@@ -27,44 +26,7 @@ const Huellas = () => {
   return (
     <>
       <Navbar />
-      <div className="w-full h-screen flex items-center justify-center bg-[#3F7E44]">
-        <div className="relative w-full lg:w-1/2">
-          {/* Carousel Images */}
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            {images.map((image, index) => (
-              <div
-                key={index}
-                className={`absolute inset-0 transition-opacity duration-500 ${
-                  index === currentIndex ? "opacity-100" : "opacity-0"
-                }`}
-              >
-                <img
-                  src={image.src}
-                  alt={image.alt}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            ))}
-          </div>
-
-          {/* Navigation Buttons */}
-          <button
-            onClick={prevSlide}
-            className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2 hover:bg-gray-700 transition duration-200"
-          >
-            &#9664;
-          </button>
-          <button
-            onClick={nextSlide}
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2 hover:bg-gray-700 transition duration-200"
-          >
-            &#9654;
-          </button>
-        </div>
-
-
-        <Swiper/>
-      </div>
+            
     </>
   );
 };
